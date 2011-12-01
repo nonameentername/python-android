ec413a6e316f627bac845d71aa4fecb6d7c87c96
werner mendizabal <nonameentername@gmail.com>
2011-11-30 22:03:14 -0600

    add script to install python on android device

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export HOSTPGEN=$ROOTDIR/hostpgen
 
 export NDK="$HOME/source/android-ndk"
 export SDK="$HOME/source/android-sdk/"
-export NDKPLATFORM="$NDK/platforms/android-5/arch-arm"
+export NDKPLATFORM="$NDK/platforms/android-9/arch-arm"
 
 export PATH="$NDK/toolchains/arm-eabi-4.4.3/prebuilt/linux-x86/bin/:$NDK:$SDK/tools:$PATH"
 
@@ -30,6 +30,7 @@ export CXX="arm-linux-androideabi-g++ $CXXFLAGS"
 export AR="arm-linux-androideabi-ar"
 export RANLIB="arm-linux-androideabi-ranlib"
 export STRIP="arm-linux-androideabi-strip --strip-unneeded"
+export BLDSHARED="arm-linux-androideabi-gcc -shared"
 export MAKE="make -j4"
 
 #export PYTHONHOME="$ROOTDIR/build"
@@ -39,5 +40,5 @@ cd $ROOTDIR/Python
 
 #sed -i "s|^INSTSONAME=\(.*.so\).*|INSTSONAME=\\1|g" Makefile
 
-$MAKE HOSTPYTHON=$HOSTPYTHON HOSTPGEN=$HOSTPGEN CROSS_COMPILE=arm-eabi- CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so
-$MAKE install HOSTPYTHON=$HOSTPYTHON HOSTPGEN=$HOSTPGEN CROSS_COMPILE=arm-eabi- CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so
+$MAKE HOSTPYTHON=$HOSTPYTHON HOSTPGEN=$HOSTPGEN BLDSHARED="$BLDSHARED" CROSS_COMPILE=arm-eabi- CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so
+$MAKE install HOSTPYTHON=$HOSTPYTHON HOSTPGEN=$HOSTPGEN BLDSHARED="$BLDSHARED" CROSS_COMPILE=arm-eabi- CROSS_COMPILE_TARGET=yes INSTSONAME=libpython2.7.so
---
+++ package.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./package.sh <namespace>"
+    exit 0
+fi
+
+ROOTDIR=`pwd`
+PACKAGE=$1
+
+yes | rm -r $ROOTDIR/release
+
+mkdir -p $ROOTDIR/release/$PACKAGE/files/python/bin
+mkdir -p $ROOTDIR/release/$PACKAGE/files/python/include
+mkdir -p $ROOTDIR/release/$PACKAGE/files/python/lib/python2.7/lib-dynload
+mkdir -p $ROOTDIR/release/$PACKAGE/files/python/lib/python2.7/config
+mkdir -p $ROOTDIR/release/$PACKAGE/extras/python
+
+cat $ROOTDIR/standalone_python.sh | sed -e s"/<PACKAGE>/$PACKAGE/g" > $ROOTDIR/release/$PACKAGE/files/python/bin/standalone_python.sh
+cp -r $ROOTDIR/build/bin/python $ROOTDIR/release/$PACKAGE/files/python/bin/python
+cp -r $ROOTDIR/build/include/* $ROOTDIR/release/$PACKAGE/files/python/include/
+cp -r $ROOTDIR/build/lib/libpython2.7.so $ROOTDIR/release/$PACKAGE/files/python/lib/libpython2.7.so
+cp -r $ROOTDIR/build/lib/python2.7/lib-dynload/* $ROOTDIR/release/$PACKAGE/files/python/lib/python2.7/lib-dynload/
+cp -r $ROOTDIR/build/lib/python2.7/config/* $ROOTDIR/release/$PACKAGE/files/python/lib/python2.7/config/
+cp -r $ROOTDIR/build/lib/python2.7/* $ROOTDIR/release/$PACKAGE/extras/python/
+
+cd $ROOTDIR/release/$PACKAGE/extras/python/
+rm `find . | grep -v "so$\|py$"`
+rm -r `find . | grep test`
+
+adb push $ROOTDIR/release/$PACKAGE/files/ /data/data/$PACKAGE/files
+adb push $ROOTDIR/release/$PACKAGE/extras/ /mnt/sdcard/$PACKAGE/extras
